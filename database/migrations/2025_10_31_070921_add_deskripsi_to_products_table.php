fb27a328e7fa4ab9e4a09af1c21e599bcf7fbfa2
valentino-rossi-hartono <230712311@students.uajy.ac.id>
2025-10-31 14:12:17 +0700

    ya kontol

--- database/migrations/2025_10_31_070921_add_deskripsi_to_products_table.php
+++ database/migrations/2025_10_31_070921_add_deskripsi_to_products_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->string('product');
+        Schema::table('products', function (Blueprint $table) {
             $table->string('deskripsi');
-            $table->timestamps();
         });
     }
 
@@ -24,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
     }
 };
